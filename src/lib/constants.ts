4a8cc934cfa282d9bbac530b1151633ef0106203
AbhinavGGarg <abhinavmgarg@gmail.com>
2026-03-21 20:09:40 -0700

    Deploy config: socket PORT compatibility

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,5 +7,5 @@ export const GLOBAL_DROP_INTERVAL_MS = 1000 * 60 * 3;
 export const GLOBAL_DROP_WINDOW_MS = 1000 * 40;
 export const GLOBAL_CLOCK_EPOCH = Date.UTC(2026, 0, 1, 0, 0, 0);
 
-export const SOCKET_PORT = Number(process.env.SOCKET_PORT ?? 4001);
+export const SOCKET_PORT = Number(process.env.PORT ?? process.env.SOCKET_PORT ?? 4001);
 export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL ?? `http://localhost:${SOCKET_PORT}`;
